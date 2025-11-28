f594cb0e7ab785eedd602891e030d071b58c3232
Vincent <vincent.ptk17@gmail.com>
2025-11-29 01:05:29 +0800

    feat: add data.ts to store datas

---
+++ lib/data.ts
@@ -0,0 +1,16 @@
+import { BiLogoFacebook } from "react-icons/bi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineYoutube } from "react-icons/ai";
+
+export const navMenus = [
+  { href: "/collections", name: "collections" },
+  { href: "/about", name: "about us" },
+  { href: "/service", name: "service" },
+  { href: "/contact", name: "contact" },
+];
+
+export const navSocialIcons = [
+    { href: "facebook.com", icon: BiLogoFacebook },
+    { href: "instagram.com", icon: AiOutlineInstagram },
+    { href: "youtube.com", icon: AiOutlineYoutube }
+];
