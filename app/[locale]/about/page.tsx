cfdd03d23f7b9ea4a3bacce4cd6d89970f506c35
Vincent <vincent.ptk17@gmail.com>
2025-12-02 09:57:39 +0800

    feat: create Collection section for about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import DynamicBreadcrumb from "@/components/DynamicBreadcrumb";
-import { count } from "@/lib/data";
+import { collections, count } from "@/lib/data";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import CountUp from "@/components/CountUp";
 import IconLine from "@/components/IconLine";
@@ -176,6 +176,38 @@ export default function About() {
       </section>
       {/* Logo Seperator */}
       <IconLine />
+      {/* Collection */}
+      <section className="w-full">
+            <div className="flex flex-col gap-10 px-8 max-w-[1575px] mx-auto">
+              <div className="flex justify-between items-center">
+                <h2 className="font-poppins text-[38px] font-medium text-black">
+                  Our Collections
+                </h2>
+                <p className="font-inter font-normal text-base text-black/60 capitalize">
+                  Decades of serving customers with a diverse collection of
+                  <br /> rugs have built a strong foundation of trust and reliability.
+                </p>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+                {collections.map((item, index) => (
+                  <div className="group flex flex-col gap-3" key={index}>
+                    <div className="aspect-4/3 relative">
+                      <Image
+                        src={item.src}
+                        alt={item.name}
+                        width={800}
+                        height={600}
+                        className="object-cover w-full h-full"
+                      />
+                    </div>
+                    <span className="font-poppins font-normal text-lg">
+                      {item.name}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
     </main>
   );
 }
